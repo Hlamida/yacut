51dbc7ee4e3a8cf11db7b9371e646a6c3c1b4a49
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-06-03 14:31:41 +0300

    Pytest 8

--- yacut/settings.py
+++ yacut/settings.py
@@ -5,3 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
