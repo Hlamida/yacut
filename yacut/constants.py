aaed2709e0850af1082c7c8d17f329765b555d83
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-06-11 18:34:10 +0300

    After 1-st review, v.3

--- yacut/constants.py
+++ yacut/constants.py
@@ -5,7 +5,7 @@ ORIGINAL_LINK_LENGTH = 256
 SHORT_LINK_LENGTH = 6
 USER_LINK_LENGHT = 16
 
-REGEX_LINK_PATTERN = 'http(s|)://\w+'
+REGEX_LINK_PATTERN = r'http(s|)://\w+'
 REGEX_SHORT_SYMBOLS = string.ascii_letters + string.digits
 
 REDIRECT_FUNCTION = 'redirect_view'
