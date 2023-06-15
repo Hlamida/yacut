3955937b1b33e932e871d3a82fd7a55ceedc1998
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-06-15 22:29:10 +0300

    After 2-nd review, v.2

--- yacut/settings.py
+++ yacut/settings.py
@@ -5,4 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_SECRET')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
